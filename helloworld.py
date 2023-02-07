4928267fe25859efc7a8afb45f6a2c1ebb20ea5b
stefan <stefanvb88@gmail.com>
2023-02-07 17:05:33 +0200

    second commit in git

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+print("Hello World!", end="")
+print("brakepoint")
+print("Hello World2!" , "hello3",sep="___")
+print('aaaadfdhhhhhhhndsadasdosaidnasdasdasjjjjjjjjjjsdasdasdasdajndoinfcdasoinasoinasoinaoisnoiasniasndisand',
+      'iasndoaisndoasindaoidasdasdsadsadsadadadadadadnd',sep="")
+print("changes")
+
+
+
